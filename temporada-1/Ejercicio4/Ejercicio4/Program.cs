2eb4d68cce71bc5598312770801147d3d56fbd97
Creator125 <85368990+Creator125@users.noreply.github.com>
2022-12-16 07:52:11 -0500

    Temas aprendidos hasta el tiempo 2:17:40 del tutorial

---
+++ temporada-1/Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Tema: Toma de desiciones
+ * 
+ * Crear un progama en donde el usuario ingresa una temperatua y de acuerdo
+ * a ella se le de una recomendacion de una ropa a usar
+*/
+using System;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una temperatuura");
+            string valor = Console.ReadLine();
+
+            int temperatura = Int32.Parse(valor);
+
+            if (temperatura < 10)
+            {
+                Console.WriteLine("Abrigate");
+            }
+            if (temperatura == 10)
+            {
+                Console.WriteLine("Hace mucho calor");
+            }
+            if (temperatura > 10)
+            {
+                Console.WriteLine("Las palomas se estan cocinando");
+            }
+
+            Console.Read();
+        }
+    }
+}
--- temporada-1/TomaDeDecisiones/TomaDeDecisiones/Program.cs
+++ temporada-1/TomaDeDecisiones/TomaDeDecisiones/Program.cs
@@ -7,6 +7,22 @@ namespace TomaDeDecisiones
     {
         static void Main(string[] args)
         {
+            int temperatura = 40;
+
+            if (temperatura < 40)
+            {
+                Console.WriteLine("Pararece enfiarse todo");
+            }
+            if(temperatura == 40)
+            {
+                Console.WriteLine("Hace mucho calor");
+            }
+            if (temperatura > 40)
+            {
+                Console.WriteLine("Las palomas se estan cocinando");
+            }
+
+            Console.Read();
         }
     }
 }
